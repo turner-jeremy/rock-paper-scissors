4e01841c7e020115fe9da66bd468fb66a4b27956
turner.jeremy <37211811+turner-jeremy@users.noreply.github.com>
2023-09-11 14:56:12 +0800

    Improve adaptivity of UI CSS
    Changed CSS from static px to adaptive vh/vw and clamp
    Changed script from computer to cat player
    Changed script from player to human

--- javascript.js
+++ javascript.js
@@ -1,87 +1,135 @@
-var playerScore = 0;
-var computerScore = 0;
-var playerSelection = "";
-var computerSelection = "";
+var humanScore = 0;
+var catScore = 0;
+var humanSelection = "";
+var catSelection = "";
 var roundNumber = 1;
 var selectionMade = false;
 var char = 0;
 var timer = 0;
+var drawGame = false;
+const pointsHTML = ["&#9312;", "&#9313;", "&#9314;", "&#9315;", "&#9316;"];
+catChoiceJpn = "";
+humanChoiceJpn = "";
 
-const roundNumberUI = document.querySelector('.player-instructions');
-const playerScoreUI = document.querySelector('#player-score');
-const computerScoreUI = document.querySelector('#computer-score');
+const roundNumberUI = document.querySelector('.human-instructions');
+const humanScoreDiv = document.querySelector('.human-score');
+const catScoreDiv = document.querySelector('.cat-score');
 const gameTitle = document.querySelector('.game-title');
-const playerChoiceUI = document.querySelector('#player-choice')
-const playerChoiceImg = playerChoiceUI.querySelector('img');
-const computerChoiceUI = document.querySelector('#computer-choice')
-const computerChoiceImg = computerChoiceUI.querySelector('img');
+const humanChoiceImg = document.getElementById('human-choice');
+const catChoiceImg = document.getElementById("cat-choice");
+const gameInfoUI = document.querySelector('.game-info');
+const gameStatusUI = document.querySelector('.game-status');
 
 const selectionContainer = document.querySelector('.selection-container');
-const playerButtons = selectionContainer.querySelectorAll('img');
+const humanButtons = selectionContainer.querySelectorAll('img');
+var catScoreUI = catScoreDiv.querySelectorAll('p');
+var humanScoreUI = humanScoreDiv.querySelectorAll('p');
 
-const playerRockPNG = "./images/rock-left.png"
-const playerPaperPNG = "./images/paper-left.png"
-const playerScissorsPNG = "./images/scissors-left.png"
-const computerRockPNG = "./images/rock-right.png"
-const computerPaperPNG = "./images/paper-right.png"
-const computerScissorsPNG = "./images/scissors-right.png"
+const humanRockPNG = "./images/rock-left.png";
+const humanPaperPNG = "./images/paper-left.png";
+const humanScissorsPNG = "./images/scissors-left.png";
+const catRockPNG = "./images/cat-rock.png";
+const catPaperPNG = "./images/cat-paper.png";
+const catScissorsPNG = "./images/cat-scissors.png";
+const blankPNG = "./images/blank.png";
 
-playerButtons.forEach((img) => {
+const startGameString1 = "最初はグー"
+const startGameString2 = "にゃんけんぽん！"
+const drawGameString = "あいこでしょ"
+
+humanButtons.forEach((img) => {
           img.addEventListener('click', () => {
                 if (selectionMade === false) {
                     selectionMade = true;
                     img.classList.add('clicked');
-                    playerSelection = img.id;
+                    humanSelection = img.id;
                     playRound();
                 }
               });
 
           });
 
-function getComputerChoice() {
-    const computerChoice = ["rock", "paper", "scissors"]
+function getCatChoice() {
+    const catChoice = ["rock", "paper", "scissors"]
     var i = Math.floor(3 * Math.random());
-    return computerChoice[i]
+    return catChoice[i]
 }
 
 function playRound() {
-    if (playerScore >= 5 || computerScore >= 5) return;
-
-    roundNumberUI.textContent = "ROUND " + roundNumber;
+    if (humanScore >= 5 || catScore >= 5) return;
+    roundNumberUI.classList.add("round");
+    roundNumberUI.textContent = "ラウンド " + roundNumber;
+    humanChoiceImg.classList.remove("winner");
+    humanChoiceImg.classList.remove("loser");
+    catChoiceImg.classList.remove("winner");
+    catChoiceImg.classList.remove("loser");
+    // humanChoiceImg.src = blankPNG;
+    // catChoiceImg.src = blankPNG;
     animateTitle();
 }
 
 function showScores() {
-    var computerSelection = getComputerChoice();
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            tiedGame(playerSelection, computerSelection);
-        } else if (computerSelection === "paper") {
-            computerWins(playerSelection, computerSelection);
-        } else if (computerSelection === "scissors") {
-            playerWins(playerSelection, computerSelection);
+    var catSelection = getCatChoice();
+
+    switch (humanSelection) {
+        case "rock":
+            humanChoiceImg.src = humanRockPNG;
+            humanChoiceJpn = "グー";
+            break;
+        case "paper":
+            humanChoiceImg.src = humanPaperPNG;
+            humanChoiceJpn = "パー";
+            break;
+        case "scissors":
+            humanChoiceImg.src = humanScissorsPNG;
+            humanChoiceJpn = "チョキ";
+            break;
+    }
+
+    switch (catSelection) {
+        case "rock":
+            catChoiceImg.src = catRockPNG;
+            catChoiceJpn = "グー";
+            break;
+        case "paper":
+            catChoiceImg.src = catPaperPNG;
+            catChoiceJpn = "パー";
+            break;
+        case "scissors":
+            catChoiceImg.src = catScissorsPNG;
+            catChoiceJpn = "チョキ";
+            break;
+    }
+
+    if (humanSelection === "rock") {
+        if (catSelection === "rock") {
+            tiedGame(humanSelection, catSelection);
+        } else if (catSelection === "paper") {
+            catWins(humanSelection, catSelection);
+        } else if (catSelection === "scissors") {
+            humanWins(humanSelection, catSelection);
         }
     }
-    if (playerSelection === "paper") {
-        if (computerSelection === "paper") {
-            tiedGame(playerSelection, computerSelection);
-        } else if (computerSelection === "scissors") {
-            computerWins(playerSelection, computerSelection);
-        } else if (computerSelection === "rock") {
-            playerWins(playerSelection, computerSelection);
+    if (humanSelection === "paper") {
+        if (catSelection === "paper") {
+            tiedGame(humanSelection, catSelection);
+        } else if (catSelection === "scissors") {
+            catWins(humanSelection, catSelection);
+        } else if (catSelection === "rock") {
+            humanWins(humanSelection, catSelection);
         }
     }
-    if (playerSelection === "scissors") {
-        if (computerSelection === "scissors") {
-            tiedGame(playerSelection, computerSelection);
-        } else if (computerSelection === "rock") {
-            computerWins(playerSelection, computerSelection);
-        } else if (computerSelection === "paper") {
-            playerWins(playerSelection, computerSelection);
+    if (humanSelection === "scissors") {
+        if (catSelection === "scissors") {
+            tiedGame(humanSelection, catSelection);
+        } else if (catSelection === "rock") {
+            catWins(humanSelection, catSelection);
+        } else if (catSelection === "paper") {
+            humanWins(humanSelection, catSelection);
         }
     }
 
-    if (playerScore === 5 || computerScore === 5) {
+    if (humanScore === 5 || catScore === 5) {
       endMatch();
     }
     roundNumber++;
@@ -93,59 +141,72 @@ function animateTitle() {
 }
 
 function endMatch() {
-    if (playerScore === 5) {
-      winner = "You won the match!";
-    } else if (computerScore === 5) {
-      winner = "The computer won the match.";
+    roundNumberUI.classList.remove("round");
+    if (humanScore === 5) {
+      winner = "おめでとう！マッチ勝ってしまったね！やったねー！";
+    } else if (catScore === 5) {
+      winner = "ニャーニャー！猫が勝者だよー！やったね、ニャンコ！";
     }
     selectionContainer.innerHTML = `<div class="selection-container">
     <h1 class="winner-announce">${winner}</h1>
-    <button onclick="window.location.reload();" id="reset-game">Reset</button>
+    <button onclick="window.location.reload();" id="reset-game">もう一回遊ぼう！</button>
 </div>`;
 }
 
-function tiedGame(playerSelection, computerSelection) {
-    console.log("It's a tie! You both chose " + playerSelection + ".")
+function tiedGame(humanSelection, catSelection) {
     displayScores()
+    drawGame = true;
+    gameInfoUI.textContent = "引き分けだね！";
+
 }
 
-function computerWins(playerSelection, computerSelection) {
-    console.log("You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + ".")
-    computerScore++
+function catWins(humanSelection, catSelection) {
+    humanChoiceImg.classList.add("loser");
+    catChoiceImg.classList.add("winner");
+    catScore++
+    drawGame = false;
     displayScores()
+    gameInfoUI.textContent = catChoiceJpn + "は" + humanChoiceJpn + "に勝る!"
 }
 
-function playerWins(playerSelection, computerSelection) {
-    console.log("You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".")
-    playerScore++
+function humanWins(humanSelection, catSelection) {
+    humanChoiceImg.classList.add("winner");
+    catChoiceImg.classList.add("loser");
+    humanScore++
+    drawGame = false;
     displayScores()
+    gameInfoUI.textContent = humanChoiceJpn + "は" + catChoiceJpn + "に勝る!"
 }
 
 function displayScores() {
-  playerScoreUI.textContent = playerScore;
-  computerScoreUI.textContent = computerScore;
+    if (catScore > 0) {
+        catScoreUI.item(catScore - 1).innerHTML = pointsHTML[catScore - 1];
+    }
+    if (humanScore >0) {
+        humanScoreUI.item(humanScore - 1).innerHTML = pointsHTML[humanScore - 1];
+    }
 }
-
-function gameInstructions() {
-  alert("First to five is the winner!")
-}
-
-// gameInstructions();
-
-
 
 function onTick() {
-    if (char <=2) {
-        const span = gameTitle.querySelectorAll('span')[char];
-        span.classList.add('animate-title');}
-        char++
+    if (char == 0) {
+        gameStatusUI.textContent = "";
+        humanChoiceImg.src = humanRockPNG;
+        catChoiceImg.src = catRockPNG;
 
-    if (char === 4) {
-        for (i=0; i<=2; i++) {
-            const span = gameTitle.querySelectorAll('span')[i]
-            span.classList.remove('animate-title');
+        if (drawGame === true) {
+            gameInfoUI.textContent = drawGameString;
+            char = 3;
+            return;
+        } else {
+        gameInfoUI.textContent = startGameString1;
         }
-        playerButtons.forEach((img) => {
+    }
+
+    if (char == 2) {
+        gameInfoUI.textContent = startGameString2;
+    }
+    if (char === 3) {
+        humanButtons.forEach((img) => {
             img.classList.remove('clicked');
           })
         clearInterval(timer);
@@ -156,4 +217,5 @@ function onTick() {
         showScores();
         return;
     }
+    char++;
 }
